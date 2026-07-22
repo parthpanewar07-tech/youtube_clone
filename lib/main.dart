433f0faf587616cfc27d17ec9b5cd089b362f826
parthpanewar07-tech <parthpanewar07@gmail.com>
2026-07-22 09:11:09 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:youtube/screens/homeScreen.dart';
-import 'package:youtube/screens/mainNavigationScreen.dart';
+import 'package:youtube/firebase_options.dart';
 import 'package:youtube/screens/shortPlayerScreen1.dart';
 import 'package:youtube/screens/splashScreen.dart';
 import 'package:youtube/screens/testScreen.dart';
-import 'package:youtube/screens/videoPlayerScreen1.dart';
 import 'package:youtube/screens/videoPlayerScreen2.dart';
 
-void main() {
+void main() async {
+  //to ensure the flutter framework is fully initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //  Start Firebase using the automatic platform options
+  //  Check if Firebase is already initialized
+  if (Firebase.apps.isEmpty) {
+    // 2. If empty, initialize it
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print("Firebase just initialized!");
+  } else {
+    // 3. If not empty, skip initialization
+    print("Firebase was already initialized.");
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => SplashScreen(),
-       // '/': (context) => MainNavigationScreen(),
+        // '/': (context) => MainNavigationScreen(),
         '/ytVid1': (context) => const TestScreen(),
         '/ytVid2': (context) => const videoPlayerScreen2(),
         '/ytShVid2': (context) => const ShortPlayerScreen1(),
